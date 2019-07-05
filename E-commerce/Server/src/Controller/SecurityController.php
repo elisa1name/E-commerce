94c0583356166e1b99cd561336c24e234f35f1e3
Amina <amina.skendraoui@epitech.eu>
2019-07-05 17:31:53 +0200

    register

---
+++ E-commerce/Server/src/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/security", name="security")
+     */
+    public function index()
+    {
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+
+     /**
+     * @Route("/inscription", name="security_registration")
+     */
+
+    public function registration() {
+
+    }
+}
