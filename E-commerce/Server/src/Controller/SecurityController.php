cab23e87eb255737fe724b712929ecc2266d6e66
hady <hady.sane@epitech.eu>
2019-07-16 12:11:28 +0200

    API register fini

--- E-commerce/Server/src/Controller/SecurityController.php
+++ E-commerce/Server/src/Controller/SecurityController.php
@@ -3,13 +3,20 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationType;
+use Symfony\Component\Validator\Constraints as Assert;
 
+use App\Form\RegistrationType;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Validation;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -24,31 +31,47 @@ class SecurityController extends AbstractController
     }
 
      /**
-     * @Route("/inscription", name="security_registration")
+     * @Route("/register", name="register", methods= {"POST"})
      */
 
-    public function registration(Request $request,ObjectManager $manager, UserPasswordEncoderInterface $encoder) {
+    public function registration(Request $request,ObjectManager $manager, UserPasswordEncoderInterface $encoder, ValidatorInterface $validator) {
         $user = new User();
+        
+        $content = json_decode(
+            $request->getContent(),true
+        );
 
-        $form = $this->createForm(RegistrationType::class, $user);
-        $form->handleRequest($request);
+        $validator = Validation::createValidator();
 
-        if($form->isSubmitted() && $form->isValid()) {
-            $hash = $encoder->encodePassword($user,$user->getPassword());
+        $constraint = new Assert\Collection(array(
+            // the keys correspond to the keys in the input array
+            'password' => new Assert\Length(array('min' => 3, 'minMessage'=>'Votre mot de passe doit contenir minimum 3 caracteres')),
+            'email' => new Assert\Email(array('message'=> 'Votre email est incorrect')),
+            'firstname' => new Assert\Length(array('min' => 5, 'minMessage'=>'Votre prenom doit contenir minimum 5 caracteres')),
+            'name' => new Assert\Length(array('min' => 5, 'minMessage'=>'Votre nom doit contenir minimum 5 caracteres')),
+        ));
 
-            $user->setPassword($hash);
-            $user->setRoles(['ROLE_ADMIN']);
-            $manager->persist($user);
-            $manager->flush();
+        $violations = $validator->validate($content, $constraint);
 
-            return $this->redirectToRoute('security_login');
-           
+        if ($violations->count() > 0) {
+            return new JsonResponse(["error" => (string)$violations], 500);
         }
+        
+        $email = $content['email'];
+        $password = $content['password'];
+        $name = $content['name'];
+        $firstname = $content['firstname'];
+       
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setEmail($email);
+        $user->setname($name);
+        $user->setRoles(['ROLE_USER']);
+        $user->setfirstname($firstname);
+        $user->setFixedDeliveryPrice(false);
+        $manager->persist($user);
+        $manager->flush();
 
-
-        return $this->render('security/registration.html.twig',[
-            'form'=> $form->createView()
-        ]);
+        return new JsonResponse(["success" => $user->getEmail(). " est bien enregistré"], 200);
     }
 
     /** 
