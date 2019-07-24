b79984ad750056b6029c5bed83bb05335ed9c3b0
hady <hady.sane@epitech.eu>
2019-07-24 17:06:07 +0200

    Crud user (en cours)

--- E-commerce/Server/src/Controller/UserController.php
+++ E-commerce/Server/src/Controller/UserController.php
@@ -6,13 +6,18 @@ use App\Entity\User;
 use App\Form\UserType;
 use FOS\RestBundle\View\View;
 use App\Repository\UserRepository;
+use Symfony\Component\Validator\Validation;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use FOS\RestBundle\Controller\Annotations as FOSRest;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use FOS\RestBundle\Controller\Annotations as FOSRest;
-
 
 class UserController extends AbstractController
 {
@@ -30,61 +35,110 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="user_new", methods={"GET","POST"})
+     * @FOSRest\Post("api/admin/users/new")
      */
-    public function new(Request $request, UserPasswordEncoderInterface $encoder): Response
+    public function new(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $hash = $encoder->encodePassword($user,$user->getPassword());
-            $user->setPassword($hash);
+        $content = json_decode(
+            $request->getContent(),true
+        );
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+        $validator = Validation::createValidator();
 
-            return $this->redirectToRoute('user_index');
+        $constraint = new Assert\Collection(array(
+            // the keys correspond to the keys in the input array
+            'password' => new Assert\Length(array('min' => 3, 'minMessage'=>'Votre mot de passe doit contenir minimum 3 caracteres')),
+            'email' => new Assert\Email(array('message'=> 'Votre email est incorrect')),
+            'firstname' => new Assert\Length(array('min' => 2, 'minMessage'=>'Votre prenom doit contenir minimum 2 caracteres')),
+            'name' => new Assert\Length(array('min' => 2, 'minMessage'=>'Votre nom doit contenir minimum 2 caracteres')),
+            'adress'=> new Assert\Length(array('min' => 2, 'minMessage'=>'Votre adresse ne contient que 2 caractère')),
+            'telephone'=> new Assert\Length(array('min' => 2, 'minMessage'=>'Votre adresse ne contient que 2 caractère')),
+        ));
+
+        $violations = $validator->validate($content, $constraint);
+
+        if ($violations->count() > 0) {
+            return View::create(["error" => (string)$violations], Response::HTTP_CONFLICT , []);
         }
 
-        return $this->render('user/new.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+        $email = $content['email'];
+        $name = $content['name'];
+        $firstname = $content['firstname'];
+        $adress= $content['adress'];
+        $telephone =$content['telephone'];
+        $password = $content['password'];
+
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setEmail($email);
+        $user->setName($name);
+        $user->setFirstname($firstname);
+        $user->setFixedDeliveryPrice(false);
+        $user->setAdress($adress);
+        $user->setTelephone($telephone);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return View::create(["success" => $user->getEmail(). " est bien enregistré"], Response::HTTP_OK , []);
+
     }
 
     /**
-     * @Route("/{id}", name="user_show", methods={"GET"})
+     * @FOSRest\Get("api/admin/users/{id}")
      */
-    public function show(User $user): Response
+    public function show(User $user)
     {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
+        return View::create($user, Response::HTTP_OK , []);
     }
 
     /**
-     * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
+     * @FOSRest\Put("api/admin/users/{id}/edit")
      */
-    public function edit(Request $request, User $user): Response
+    public function edit(Request $request, User $user, UserPasswordEncoderInterface $encoder)
     {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+        $content = json_decode(
+           $request->getContent(),true
+        );
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+        $validator = Validation::createValidator();
 
-            return $this->redirectToRoute('user_index', [
-                'id' => $user->getId(),
-            ]);
+        $constraint = new Assert\Collection(array(
+            // the keys correspond to the keys in the input array
+            'password' => new Assert\Length(array('min' => 3, 'minMessage'=>'Votre mot de passe doit contenir minimum 3 caracteres')),
+            'email' => new Assert\Email(array('message'=> 'Votre email est incorrect')),
+            'firstname' => new Assert\Length(array('min' => 2, 'minMessage'=>'Votre prenom doit contenir minimum 2 caracteres')),
+            'name' => new Assert\Length(array('min' => 2, 'minMessage'=>'Votre nom doit contenir minimum 2 caracteres')),
+            'adress'=> new Assert\Length(array('min' => 2, 'minMessage'=>'fgffffd')),
+            'telephone'=> new Assert\Length(array('min' => 2, 'minMessage'=>'fgffffd')),
+        ));
+
+        $violations = $validator->validate($content, $constraint);
+
+        if ($violations->count() > 0) {
+            return View::create(["error" => (string)$violations], Response::HTTP_CONFLICT , []);
         }
 
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+        $email = $content['email'];
+        $name = $content['name'];
+        $firstname = $content['firstname'];
+        $adress= $content['adress'];
+        $telephone =$content['telephone'];
+        $password = $content['password'];
+
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setEmail($email);
+        $user->setName($name);
+        $user->setFirstname($firstname);
+        $user->setFixedDeliveryPrice(false);
+        $user->setAdress($adress);
+        $user->setTelephone($telephone);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($user);
+        $manager->flush();
+
+        return View::create(["success" => $user->getEmail(). " profil modifier"], Response::HTTP_OK , []);
     }
     
     /**
